bfabbd08d06b076c69ac3ec59826fb18050bbf32
seonghaejo <jsh990324@snu.ac.kr>
2023-12-29 00:21:19 +0900

    Initial setting (spring initializer)

---
+++ src/main/kotlin/com/wafflestudio/toyproject/waffle5gramserver/Waffle5gramserverApplication.kt
@@ -0,0 +1,11 @@
+package com.wafflestudio.toyproject.waffle5gramserver
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class Waffle5gramserverApplication
+
+fun main(args: Array<String>) {
+	runApplication<Waffle5gramserverApplication>(*args)
+}
---
+++ src/test/kotlin/com/wafflestudio/toyproject/waffle5gramserver/Waffle5gramserverApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.wafflestudio.toyproject.waffle5gramserver
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class Waffle5gramserverApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
