8a3421115ac24c4eca3fb4acbb86283ab91b8ddf
suhyuk <isuh88@snu.ac.kr>
2024-01-02 16:17:53 +0900

    feat: reply 디렉토리 세팅

---
+++ src/main/kotlin/com/wafflestudio/toyproject/waffle5gramserver/reply/controller/ReplyController.kt
@@ -0,0 +1,7 @@
+package com.wafflestudio.toyproject.waffle5gramserver.reply.controller
+
+import org.springframework.web.bind.annotation.RestController
+
+@RestController
+class ReplyController {
+}
