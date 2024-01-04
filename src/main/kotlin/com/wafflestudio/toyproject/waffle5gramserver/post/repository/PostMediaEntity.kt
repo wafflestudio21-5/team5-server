bafcfafcf604c3efbd5e272e62c3fbe9eb9abd7a
rayark1 <max980203@snu.ac.kr>
2024-01-05 00:15:55 +0900

    feat: PostMediaEntity 구현

---
+++ src/main/kotlin/com/wafflestudio/toyproject/waffle5gramserver/post/repository/PostMediaEntity.kt
@@ -0,0 +1,28 @@
+package com.wafflestudio.toyproject.waffle5gramserver.post.repository
+
+import jakarta.persistence.*
+
+@Entity(name = "post_medias")
+class PostMediaEntity (
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    var id: Long = 0L,
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "post_id", nullable = false)
+    var post: PostEntity,
+
+    @Column(name = "media_url", nullable = false)
+    var mediaUrl: String = "",
+
+    @Column(name = "media_type", nullable = false)
+    @Enumerated(EnumType.STRING)
+    var mediaType: MediaType = MediaType.IMAGE,
+
+    @Column(name = "media_order", nullable = false)
+    var mediaOrder: Int = 0,
+    )
+
+enum class MediaType {
+    IMAGE, VIDEO
+}
