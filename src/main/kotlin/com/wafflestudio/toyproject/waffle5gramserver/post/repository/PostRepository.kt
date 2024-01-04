7aaeb58018b25943c3806e6e1623d6b41eded825
rayark1 <max980203@snu.ac.kr>
2024-01-04 15:34:52 +0900

    feat: PostRepository JpaRepository interface 추가

---
+++ src/main/kotlin/com/wafflestudio/toyproject/waffle5gramserver/post/repository/PostRepository.kt
@@ -0,0 +1,7 @@
+package com.wafflestudio.toyproject.waffle5gramserver.post.repository
+
+import org.springframework.data.jpa.repository.JpaRepository
+
+
+interface PostRepository : JpaRepository<PostEntity, Long> {
+}
