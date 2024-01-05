9c7a77675dda9bc46890cbd7e9c1de792ec711a0
suhyuk <isuh88@snu.ac.kr>
2024-01-05 14:29:00 +0900

    fix: ReplyEntity 구조 변경

--- src/main/kotlin/com/wafflestudio/toyproject/waffle5gramserver/reply/repository/ReplyEntity.kt
+++ src/main/kotlin/com/wafflestudio/toyproject/waffle5gramserver/reply/repository/ReplyEntity.kt
@@ -3,6 +3,7 @@ package com.wafflestudio.toyproject.waffle5gramserver.reply.repository
 import com.wafflestudio.toyproject.waffle5gramserver.BaseModificationAuditingEntity
 // import com.wafflestudio.toyproject.waffle5gramserver.comment.repository.CommentEntity
 import com.wafflestudio.toyproject.waffle5gramserver.user.repository.UserEntity
+import jakarta.persistence.Column
 import jakarta.persistence.Entity
 import jakarta.persistence.FetchType
 import jakarta.persistence.JoinColumn
@@ -10,13 +11,14 @@ import jakarta.persistence.ManyToOne
 
 @Entity(name = "replies")
 class ReplyEntity (
+    @Column(columnDefinition = "LONGTEXT")
     var content: String,
     // @ManyToOne(fetch = FetchType.LAZY)
     // @JoinColumn(name = "comment_id")
     // val comment: CommentEntity,
     @ManyToOne(fetch = FetchType.LAZY)
-    @JoinColumn(name = "author_id")
-    val author: UserEntity,
+    @JoinColumn(name = "user_id")
+    val user: UserEntity,
 ) : BaseModificationAuditingEntity() {
     fun updateContent(content: String) {
         this.content = content
