430406281ddfcdd6651d2c3c0c51dfe38049c8f7
suhyuk <isuh88@snu.ac.kr>
2024-01-05 16:14:48 +0900

    fix: required 속성 반영 및 도메인 구조 반영

--- src/main/kotlin/com/wafflestudio/toyproject/waffle5gramserver/reply/repository/ReplyEntity.kt
+++ src/main/kotlin/com/wafflestudio/toyproject/waffle5gramserver/reply/repository/ReplyEntity.kt
@@ -1,6 +1,7 @@
 package com.wafflestudio.toyproject.waffle5gramserver.reply.repository
 
 import com.wafflestudio.toyproject.waffle5gramserver.BaseModificationAuditingEntity
+import com.wafflestudio.toyproject.waffle5gramserver.comment.repository.CommentEntity
 // import com.wafflestudio.toyproject.waffle5gramserver.comment.repository.CommentEntity
 import com.wafflestudio.toyproject.waffle5gramserver.user.repository.UserEntity
 import jakarta.persistence.Column
@@ -13,14 +14,20 @@ import jakarta.persistence.ManyToOne
 class ReplyEntity (
     @Column(columnDefinition = "LONGTEXT")
     var content: String,
-    // @ManyToOne(fetch = FetchType.LAZY)
-    // @JoinColumn(name = "comment_id")
-    // val comment: CommentEntity,
+
     @ManyToOne(fetch = FetchType.LAZY)
-    @JoinColumn(name = "user_id")
+    @JoinColumn(name = "user_id", nullable = false)
     val user: UserEntity,
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "comment_id", nullable = false)
+    val comment: CommentEntity,
 ) : BaseModificationAuditingEntity() {
     fun updateContent(content: String) {
         this.content = content
+    }
+
+    fun replyId(): Long {
+        return this.id
     }
 }
