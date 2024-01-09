fc75080b3153ce346f275f8b4b435ef0bef54e2c
rayark1 <max980203@snu.ac.kr>
2024-01-09 19:07:34 +0900

    feat: BaseAuditingEntity 상속 적용

--- src/main/kotlin/com/wafflestudio/toyproject/waffle5gramserver/post/repository/PostEntity.kt
+++ src/main/kotlin/com/wafflestudio/toyproject/waffle5gramserver/post/repository/PostEntity.kt
@@ -1,5 +1,7 @@
 package com.wafflestudio.toyproject.waffle5gramserver.post.repository
 
+import com.wafflestudio.toyproject.waffle5gramserver.BaseAuditingEntity
+import com.wafflestudio.toyproject.waffle5gramserver.comment.repository.CommentEntity
 import jakarta.persistence.*
 import org.hibernate.annotations.CreationTimestamp
 import org.hibernate.annotations.UpdateTimestamp
@@ -8,28 +10,26 @@ import java.time.LocalDateTime
 
 @Entity(name = "posts")
 class PostEntity (
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    var id: Long = 0L,
-
-    @ManyToOne(fetch = FetchType.LAZY)
-    @JoinColumn(name = "user_id", nullable = false)
-    var user: UserEntity,
-
-    @Lob
-    var caption: String = "",
+    @Column(name = "content", nullable = false)
+    var content: String = "",
 
     @Column(name = "like_count_displayed", nullable = false)
-    var likeCountDisplayed: Boolean = false,
+    var likeCountDisplayed: Boolean = true,
 
     @Column(name = "comment_displayed", nullable = false)
-    var commentDisplayed: Boolean = false,
-
-    @CreationTimestamp
-    @Column(name = "created_at", nullable = false, updatable = false)
-    var createdAt: LocalDateTime = LocalDateTime.now(),
+    var commentDisplayed: Boolean = true,
 
     @UpdateTimestamp
     @Column(name = "modified_at", nullable = false)
     var modifiedAt: LocalDateTime = LocalDateTime.now(),
-)
+
+    @OneToMany(mappedBy = "post", cascade = [CascadeType.ALL], orphanRemoval = true)
+    var medias: MutableList<PostMediaEntity> = mutableListOf(),
+
+    @OneToMany(mappedBy = "post", cascade = [CascadeType.ALL], orphanRemoval = true)
+    var comments: MutableList<CommentEntity> = mutableListOf(),
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id", nullable = false)
+    var user: UserEntity,
+    ) : BaseAuditingEntity()
