494097ff5936552d938ca488c369b11825fb5973
suhyuk <isuh88@snu.ac.kr>
2024-01-12 22:14:25 +0900

    feat: PostEntity를 PostDetail로 변환

--- src/main/kotlin/com/wafflestudio/toyproject/waffle5gramserver/post/mapper/PostMapper.kt
+++ src/main/kotlin/com/wafflestudio/toyproject/waffle5gramserver/post/mapper/PostMapper.kt
@@ -2,6 +2,7 @@ package com.wafflestudio.toyproject.waffle5gramserver.post.mapper
 
 import com.wafflestudio.toyproject.waffle5gramserver.post.repository.PostEntity
 import com.wafflestudio.toyproject.waffle5gramserver.post.service.PostBrief
+import com.wafflestudio.toyproject.waffle5gramserver.post.service.PostDetail
 
 class PostMapper {
     companion object {
@@ -10,6 +11,25 @@ class PostMapper {
                 id = entity.id,
                 content = entity.content,
                 createdAt = entity.createdAt
+            )
+        }
+
+        fun toPostDetailDTO(entity: PostEntity, liked: Boolean): PostDetail {
+//            val likeCount: Int = entity.likes.size
+            val commentCount: Int = entity.comments.size
+
+            return PostDetail(
+                id = entity.id,
+                userId = entity.user.id,
+                content = entity.content,
+                liked = liked,
+                likeCount = 0,
+                commentCount = commentCount,
+                comments = entity.comments,
+                disableComment = entity.commentDisplayed,
+                hideLike = entity.likeCountDisplayed,
+                createdAt = entity.createdAt,
+                updatedAt = entity.modifiedAt
             )
         }
     }
