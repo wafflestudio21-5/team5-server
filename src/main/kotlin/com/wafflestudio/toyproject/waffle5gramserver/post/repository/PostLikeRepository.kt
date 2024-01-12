1b771231ba0a5442d8f834acad4e402ad553bd7e
suhyuk <isuh88@snu.ac.kr>
2024-01-12 22:13:52 +0900

    feat: post like count 계산, post 정렬

--- src/main/kotlin/com/wafflestudio/toyproject/waffle5gramserver/post/repository/PostLikeRepository.kt
+++ src/main/kotlin/com/wafflestudio/toyproject/waffle5gramserver/post/repository/PostLikeRepository.kt
@@ -5,5 +5,6 @@ import org.springframework.data.jpa.repository.JpaRepository
 interface PostLikeRepository : JpaRepository<PostLikeEntity, Long> {
     fun findByPostIdAndUserId(postId: Long, userId: Long): PostLikeEntity?
     fun countByPostId(postId: Long): Long
+    fun countByPostIdAndUserId(postId: Long, userId: Long): Long
     fun deleteAllByPostId(postId: Long)
 }
--- src/main/kotlin/com/wafflestudio/toyproject/waffle5gramserver/post/repository/PostRepository.kt
+++ src/main/kotlin/com/wafflestudio/toyproject/waffle5gramserver/post/repository/PostRepository.kt
@@ -1,6 +1,9 @@
 package com.wafflestudio.toyproject.waffle5gramserver.post.repository
 
+import org.springframework.data.domain.Page
+import org.springframework.data.domain.Pageable
 import org.springframework.data.jpa.repository.JpaRepository
 
 interface PostRepository : JpaRepository<PostEntity, Long> {
+    fun findByUserIdOrderByCreatedAtDesc(userId: Long, pageable: Pageable): Page<PostEntity>
 }
