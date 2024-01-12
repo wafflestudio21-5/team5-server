8e747517e3d12f97c0125ad653c37e77d901dc3a
rayark1 <max980203@snu.ac.kr>
2024-01-12 17:50:17 +0900

    feat: PostBriefDTO를 위한 Mapper 클래스 작성

---
+++ src/main/kotlin/com/wafflestudio/toyproject/waffle5gramserver/post/mapper/PostMapper.kt
@@ -0,0 +1,16 @@
+package com.wafflestudio.toyproject.waffle5gramserver.post.mapper
+
+import com.wafflestudio.toyproject.waffle5gramserver.post.repository.PostEntity
+import com.wafflestudio.toyproject.waffle5gramserver.post.service.PostBrief
+
+class PostMapper {
+    companion object {
+        fun toPostBriefDTO(entity: PostEntity): PostBrief {
+            return PostBrief(
+                id = entity.id,
+                content = entity.content,
+                createdAt = entity.createdAt
+            )
+        }
+    }
+}
