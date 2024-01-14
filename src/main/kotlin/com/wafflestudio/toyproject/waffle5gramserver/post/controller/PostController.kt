5765fdda01f5706157282c9a010afd42f8073549
rayark1 <max980203@snu.ac.kr>
2024-01-14 11:10:57 +0900

    feat: PostException에 대한 핸들링 추가

--- src/main/kotlin/com/wafflestudio/toyproject/waffle5gramserver/post/controller/PostController.kt
+++ src/main/kotlin/com/wafflestudio/toyproject/waffle5gramserver/post/controller/PostController.kt
@@ -1,10 +1,8 @@
 package com.wafflestudio.toyproject.waffle5gramserver.post.controller
 
+import com.wafflestudio.toyproject.waffle5gramserver.post.service.*
 import com.wafflestudio.toyproject.waffle5gramserver.user.service.InstagramUser
 
-import com.wafflestudio.toyproject.waffle5gramserver.post.service.PostBrief
-import com.wafflestudio.toyproject.waffle5gramserver.post.service.PostDetail
-import com.wafflestudio.toyproject.waffle5gramserver.post.service.PostService
 import org.springframework.http.ResponseEntity
 import org.springframework.security.core.annotation.AuthenticationPrincipal
 import org.springframework.web.bind.annotation.*
@@ -37,5 +35,16 @@ class PostController (private val postService: PostService)
     @PutMapping("/posts/{postId}")
     fun updatePost(@AuthenticationPrincipal user: InstagramUser, @PathVariable postId: Long, @RequestParam content: String) : PostBrief {
         return postService.patch(postId = postId, content =  content, userId = user.id)
+    }
+
+    @ExceptionHandler(PostException::class)
+    fun handleException(e: PostException): ResponseEntity<Any> {
+        when (e) {
+            is PostNotFoundException -> return ResponseEntity.notFound().build()
+            is PostNotAuthorizedException -> return ResponseEntity.status(403).build()
+            is UserNotFoundException -> return ResponseEntity.status(404).build()
+            is PostAlreadyLikedException, is PostNotLikedException -> return ResponseEntity.status(409).build()
+            else -> return ResponseEntity.status(500).build()
+        }
     }
 }
