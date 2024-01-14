bd085491d0115daa97d4e2e70bbbadb55476b27b
rayark1 <max980203@snu.ac.kr>
2024-01-14 15:21:46 +0900

    refactor: PostController의 에러핸들링, response type 통일

--- src/main/kotlin/com/wafflestudio/toyproject/waffle5gramserver/post/controller/PostController.kt
+++ src/main/kotlin/com/wafflestudio/toyproject/waffle5gramserver/post/controller/PostController.kt
@@ -2,39 +2,50 @@ package com.wafflestudio.toyproject.waffle5gramserver.post.controller
 
 import com.wafflestudio.toyproject.waffle5gramserver.post.service.*
 import com.wafflestudio.toyproject.waffle5gramserver.user.service.InstagramUser
+import org.springframework.http.HttpStatus
 
 import org.springframework.http.ResponseEntity
 import org.springframework.security.core.annotation.AuthenticationPrincipal
 import org.springframework.web.bind.annotation.*
 
 @RestController
+@RequestMapping("/api/v1")
 class PostController (private val postService: PostService)
 {
     @PostMapping("/posts")
     fun createPost(@AuthenticationPrincipal user: InstagramUser,
-                   @RequestParam("content") content: String,
-                   @RequestParam("hideComments", defaultValue = "false") hideComments: Boolean,
-                   @RequestParam("hideLikes", defaultValue = "false") hideLikes: Boolean,
-                   @RequestParam("files") files: List<String>,
-                   ): ResponseEntity<Any> {
+                   @RequestBody request: CreatePostRequest): ResponseEntity<PostBrief> {
         // 게시물 생성 로직 처리
-        val post = postService.create(content = content, fileUrls = files, disableComment = hideComments, hideLike =  hideLikes, userId = user.id)
-        return ResponseEntity.ok(post)
+        val post = postService.create(
+            content = request.content,
+            fileUrls = request.files,
+            disableComment = request.hideComments,
+            hideLike =  request.hideLikes,
+            userId = user.id)
+        return ResponseEntity.status(HttpStatus.CREATED).body(post)
+
     }
 
     @GetMapping("/posts/{postId}")
-    fun getPost(@AuthenticationPrincipal user: InstagramUser, @PathVariable postId: Long) : PostDetail {
-        return postService.get(postId = postId, userId = user.id)
+    fun getPost(@AuthenticationPrincipal user: InstagramUser, @PathVariable postId: Long) : ResponseEntity<PostDetail> {
+        val postDetail = postService.get(postId = postId, userId = user.id)
+        return ResponseEntity.ok(postDetail)
     }
 
     @DeleteMapping("/posts/{postId}")
-    fun deletePost(@AuthenticationPrincipal user: InstagramUser, @PathVariable postId: Long) : Unit {
-        return postService.delete(postId = postId, userId = user.id)
+    fun deletePost(@AuthenticationPrincipal user: InstagramUser, @PathVariable postId: Long) : ResponseEntity<Unit> {
+        postService.delete(postId = postId, userId = user.id)
+        return ResponseEntity.ok().build()
     }
 
     @PutMapping("/posts/{postId}")
-    fun updatePost(@AuthenticationPrincipal user: InstagramUser, @PathVariable postId: Long, @RequestParam content: String) : PostBrief {
-        return postService.patch(postId = postId, content =  content, userId = user.id)
+    fun updatePost(@AuthenticationPrincipal user: InstagramUser, @PathVariable postId: Long, @RequestBody request: UpdatePostRequest) : ResponseEntity<PostBrief> {
+        val updatedPost = postService.patch(
+            postId = postId,
+            content = request.content,
+            user.id
+        )
+        return ResponseEntity.ok(updatedPost)
     }
 
     @ExceptionHandler(PostException::class)
@@ -48,3 +59,14 @@ class PostController (private val postService: PostService)
         }
     }
 }
+
+data class CreatePostRequest(
+    val content: String,
+    val hideComments: Boolean = false,
+    val hideLikes: Boolean = false,
+    val files: List<String>
+)
+
+data class UpdatePostRequest(
+    val content: String
+)
