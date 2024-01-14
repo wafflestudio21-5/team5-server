4fa12a80a48e5c267b2b88a78b9521d4f8ff30e1
suhyuk <isuh88@snu.ac.kr>
2024-01-15 01:43:35 +0900

    feat: comment 엔티티 임시 추가

--- src/main/kotlin/com/wafflestudio/toyproject/waffle5gramserver/comment/repository/CommentEntity.kt
+++ src/main/kotlin/com/wafflestudio/toyproject/waffle5gramserver/comment/repository/CommentEntity.kt
@@ -1,4 +1,33 @@
 package com.wafflestudio.toyproject.waffle5gramserver.comment.repository
 
-class CommentEntity {
-}
+import com.wafflestudio.toyproject.waffle5gramserver.post.repository.PostEntity
+import com.wafflestudio.toyproject.waffle5gramserver.user.repository.UserEntity
+import jakarta.persistence.Column
+import jakarta.persistence.Entity
+import jakarta.persistence.FetchType
+import jakarta.persistence.GeneratedValue
+import jakarta.persistence.GenerationType
+import jakarta.persistence.Id
+import jakarta.persistence.JoinColumn
+import jakarta.persistence.ManyToOne
+import java.util.Date
+
+@Entity(name = "comments")
+class CommentEntity(
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    var id: Long = 0L,
+    @Column(nullable = false)
+    val text: String = "",
+
+    @Column(nullable = false)
+    val createdAt: Date,
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "post_id", nullable = false)
+    val post: PostEntity,
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id", nullable = false)
+    val user: UserEntity,
+)
