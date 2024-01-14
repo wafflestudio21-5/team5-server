008731e7d892f375e8e8780244832b7aafe18df7
jj1kim <jeewonbob@gmail.com>
2024-01-14 20:58:22 +0900

    refactor: 에러 처리&응답 처리를 위한 디렉토리 구조 설계

---
+++ src/main/kotlin/com/wafflestudio/toyproject/waffle5gramserver/global/error_handling/ErrorResponse.kt
@@ -0,0 +1,4 @@
+package com.wafflestudio.toyproject.waffle5gramserver.global.error_handling
+
+class ErrorResponse {
+}
---
+++ src/main/kotlin/com/wafflestudio/toyproject/waffle5gramserver/global/result_handling/ResultCode.kt
@@ -0,0 +1,4 @@
+package com.wafflestudio.toyproject.waffle5gramserver.global.result_handling
+
+enum class ResultCode {
+}
