1fb9ec8a873c232fdc0a33f694939982c7402f7a
suhyuk <isuh88@snu.ac.kr>
2024-01-15 01:48:19 +0900

    feat: 생성일자 기준 홈 피드 페이지네이션

--- src/main/kotlin/com/wafflestudio/toyproject/waffle5gramserver/feed/service/FeedServiceImpl.kt
+++ src/main/kotlin/com/wafflestudio/toyproject/waffle5gramserver/feed/service/FeedServiceImpl.kt
@@ -1,32 +1,32 @@
 package com.wafflestudio.toyproject.waffle5gramserver.feed.service
 
 import com.wafflestudio.toyproject.waffle5gramserver.post.mapper.PostMapper
-import com.wafflestudio.toyproject.waffle5gramserver.post.repository.PostEntity
-import com.wafflestudio.toyproject.waffle5gramserver.post.repository.PostLikeRepository
 import com.wafflestudio.toyproject.waffle5gramserver.post.repository.PostRepository
 import com.wafflestudio.toyproject.waffle5gramserver.post.service.PostDetail
 import org.springframework.data.domain.Page
 import org.springframework.data.domain.Pageable
 import org.springframework.stereotype.Service
+import org.springframework.transaction.annotation.Transactional
 
 @Service
-class FeedServiceImpl (
+class FeedServiceImpl(
     private val postRepository: PostRepository,
-    private val postLikeRepository: PostLikeRepository
 ) : FeedService {
 
+    @Transactional(readOnly = true)
     override fun getHomeFeed(userId: Long, pageable: Pageable): Page<PostDetail> {
-        val posts = postRepository.findByUserIdOrderByCreatedAtDesc(userId, pageable)
-//        val posts = postRepository.findAllByFollowingAndUnliked(userId, pageable)
+        val postsList = postRepository.findAllByUserIdIsNotOrderByCreatedAtDesc(userId, pageable)
 
-        return posts.map { post:PostEntity ->
-            val liked = postLikeRepository.countByPostIdAndUserId(post.id, userId) > 0
+        val postDetailsList = postsList.map { post ->
+//            val liked = postLikeRepository.exists(userId, post.id)
+            val liked = false
             PostMapper.toPostDetailDTO(post, liked)
         }
+
+        return postDetailsList
     }
 
     override fun getFeedWithPostId(postId: Long): List<PostDetail> {
         TODO("Not yet implemented")
     }
-
 }
