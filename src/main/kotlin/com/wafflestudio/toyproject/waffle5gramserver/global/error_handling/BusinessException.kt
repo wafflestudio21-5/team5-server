73aa765b96e775b03202c295725ca883e48680cc
jj1kim <jeewonbob@gmail.com>
2024-01-17 04:23:28 +0900

    docs: 설명 문구 수정

--- src/main/kotlin/com/wafflestudio/toyproject/waffle5gramserver/global/error_handling/BusinessException.kt
+++ src/main/kotlin/com/wafflestudio/toyproject/waffle5gramserver/global/error_handling/BusinessException.kt
@@ -3,6 +3,7 @@ package com.wafflestudio.toyproject.waffle5gramserver.global.error_handling
 /*
 Business layer에서 발생하는 에러들에 대해서 공통적으로 처리해주기 위한 Exception입니다.
 각각의 Exception을 정의하실 때 BusinessException을 상속받으신 뒤, super()를 이용해서 원하는 형태의 생성자를 사용해주시면 됩니다.
+일반적으로 쓰이는 몇개의 Exception들에 대해서는 미리 만들어놓았으니, 비슷한 방식으로 작업하셔도 됩니다.
  */
 open class BusinessException : RuntimeException {
 
