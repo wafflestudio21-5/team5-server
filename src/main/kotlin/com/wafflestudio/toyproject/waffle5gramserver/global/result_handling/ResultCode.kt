849cdee0e77237f5599d477abbf774db76d0f07d
jj1kim <jeewonbob@gmail.com>
2024-01-17 03:22:18 +0900

    refactor: ResultCode 틀 작성

--- src/main/kotlin/com/wafflestudio/toyproject/waffle5gramserver/global/result_handling/ResultCode.kt
+++ src/main/kotlin/com/wafflestudio/toyproject/waffle5gramserver/global/result_handling/ResultCode.kt
@@ -1,3 +1,22 @@
 package com.wafflestudio.toyproject.waffle5gramserver.global.result_handling
 
-enum class ResultCode
+// ErrorCode와 동일한 방식으로 Result 응답을 정의합니다.
+enum class ResultCode(
+    val status: Int,
+    val code: String,
+    val message: String,
+) {
+
+    // User Result
+    REGISTER_SUCCESS(200, "M001", "회원가입에 성공하였습니다."), // 예시
+
+    // Post Result
+
+    // Follow Result
+
+    // Feed Result
+
+    // Search Result
+
+    // other else
+}
