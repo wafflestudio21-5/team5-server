e24c92f09bc99a974412716d932c3da9433662b3
rayark1 <max980203@snu.ac.kr>
2024-01-17 12:17:23 +0900

    feat: delete에 대한 HTTP 상태코드 변경

--- src/main/kotlin/com/wafflestudio/toyproject/waffle5gramserver/comment/controller/CommentController.kt
+++ src/main/kotlin/com/wafflestudio/toyproject/waffle5gramserver/comment/controller/CommentController.kt
@@ -15,7 +15,6 @@ import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.PutMapping
 import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
-import org.springframework.web.bind.annotation.RequestParam
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
@@ -58,7 +57,7 @@ class CommentController(
         @PathVariable("commentId") commentId: Long,
     ): ResponseEntity<Unit> {
         commentService.delete(commentId, user.id)
-        return ResponseEntity.ok().build()
+        return ResponseEntity.noContent().build()
     }
 }
 
