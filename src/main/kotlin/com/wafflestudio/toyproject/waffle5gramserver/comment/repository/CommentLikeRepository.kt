02cb40ad4feca323a86d1c5932af612b7de9b00d
rayark1 <max980203@snu.ac.kr>
2024-01-17 11:58:02 +0900

    feat: commentLike 서비스를 위한 엔티티, JPA Repository 추가

---
+++ src/main/kotlin/com/wafflestudio/toyproject/waffle5gramserver/comment/repository/CommentLikeRepository.kt
@@ -0,0 +1,7 @@
+package com.wafflestudio.toyproject.waffle5gramserver.comment.repository
+
+import org.springframework.data.jpa.repository.JpaRepository
+
+interface CommentLikeRepository : JpaRepository<CommentLikeEntity, Long> {
+    fun countByCommentId(commentId: Long): Long
+}
