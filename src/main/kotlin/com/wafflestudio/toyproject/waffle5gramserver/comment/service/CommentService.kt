b1244649064b5cbc55c20e6c8d4838ce505da9b7
rayark1 <max980203@snu.ac.kr>
2024-01-17 12:01:15 +0900

    style: ktlint indent 적용

--- src/main/kotlin/com/wafflestudio/toyproject/waffle5gramserver/comment/service/CommentService.kt
+++ src/main/kotlin/com/wafflestudio/toyproject/waffle5gramserver/comment/service/CommentService.kt
@@ -1,8 +1,28 @@
 package com.wafflestudio.toyproject.waffle5gramserver.comment.service
 
+import org.springframework.data.domain.Page
+import org.springframework.data.domain.Pageable
+
 interface CommentService {
-    fun getComments()
-    fun createComment(postId: Long, content: String): CommentBrief
-    fun updateComment(commentId: Long, content: String): CommentBrief
-    fun deleteComment(commentId: Long)
+    fun getComments(
+        postId: Long,
+        pageable: Pageable,
+    ): Page<Comment>
+
+    fun create(
+        postId: Long,
+        content: String,
+        userId: Long,
+    ): Comment
+
+    fun patch(
+        commentId: Long,
+        content: String,
+        userId: Long,
+    ): Comment
+
+    fun delete(
+        commentId: Long,
+        userId: Long,
+    )
 }
