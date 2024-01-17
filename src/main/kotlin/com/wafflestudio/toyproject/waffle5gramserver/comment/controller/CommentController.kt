b253f4b9bbcf5d61b87d94490cafac21e755d933
rayark1 <max980203@snu.ac.kr>
2024-01-17 11:59:39 +0900

    feat: 페이지네이션 적용한 CommentController 구현

--- src/main/kotlin/com/wafflestudio/toyproject/waffle5gramserver/comment/controller/CommentController.kt
+++ src/main/kotlin/com/wafflestudio/toyproject/waffle5gramserver/comment/controller/CommentController.kt
@@ -1,6 +1,13 @@
 package com.wafflestudio.toyproject.waffle5gramserver.comment.controller
 
+import com.wafflestudio.toyproject.waffle5gramserver.comment.service.Comment
 import com.wafflestudio.toyproject.waffle5gramserver.comment.service.CommentService
+import com.wafflestudio.toyproject.waffle5gramserver.user.service.InstagramUser
+import org.springframework.data.domain.Page
+import org.springframework.data.domain.Pageable
+import org.springframework.http.HttpStatus
+import org.springframework.http.ResponseEntity
+import org.springframework.security.core.annotation.AuthenticationPrincipal
 import org.springframework.web.bind.annotation.DeleteMapping
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PathVariable
@@ -17,31 +24,42 @@ class CommentController(
     private val commentService: CommentService,
 ) {
     @GetMapping("/posts/{postId}/comments")
-    fun getComments(
+    fun getCommentsByPost(
         @PathVariable("postId") postId: Long,
-        @RequestParam(defaultValue = "1", required = false) page: Long,
-        @RequestParam(defaultValue = "10", required = false) limit: Long,
-    ) {
-        TODO()
+        pageable: Pageable,
+    ): ResponseEntity<Page<Comment>> {
+        val comments = commentService.getComments(postId, pageable)
+        return ResponseEntity.ok(comments)
     }
+
     @PostMapping("/posts/{postId}/comments")
     fun createComment(
+        @AuthenticationPrincipal user: InstagramUser,
         @PathVariable("postId") postId: Long,
         @RequestBody content: String,
-    ) {
-        TODO()
+    ): ResponseEntity<Comment> {
+        val comment = commentService.create(postId, content, user.id)
+        return ResponseEntity.status(HttpStatus.CREATED).body(comment)
     }
+
     @PutMapping("/comments/{commentId}")
     fun updateComment(
+        @AuthenticationPrincipal user: InstagramUser,
         @PathVariable("commentId") commentId: Long,
         @RequestBody content: String,
-    ) {
-        TODO()
+    ): ResponseEntity<Comment> {
+        val comment = commentService.patch(commentId, content, user.id)
+        return ResponseEntity.ok(comment)
     }
+
     @DeleteMapping("/comments/{commentId}")
     fun deleteComment(
+        @AuthenticationPrincipal user: InstagramUser,
         @PathVariable("commentId") commentId: Long,
-    ) {
-        TODO()
+    ): ResponseEntity<Unit> {
+        commentService.delete(commentId, user.id)
+        return ResponseEntity.ok().build()
     }
 }
+
+
