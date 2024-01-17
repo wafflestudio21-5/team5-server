f7c0f98e7264c8a55914c63f64b2800cd2f49d1b
suhyuk <isuh88@snu.ac.kr>
2024-01-17 20:59:43 +0900

    feat: download, delete 구현

--- src/main/kotlin/com/wafflestudio/toyproject/waffle5gramserver/utils/S3ImageUpload.kt
+++ src/main/kotlin/com/wafflestudio/toyproject/waffle5gramserver/utils/S3ImageUpload.kt
@@ -2,25 +2,34 @@ package com.wafflestudio.toyproject.waffle5gramserver.utils
 
 import com.amazonaws.services.s3.AmazonS3Client
 import com.amazonaws.services.s3.model.ObjectMetadata
+import org.springframework.beans.factory.annotation.Value
 import org.springframework.web.multipart.MultipartFile
 
 class S3ImageUpload(
     private val amazonS3Client: AmazonS3Client,
-    private val bucket: String
 ) {
+    @Value("\${cloud.aws.s3.bucket}")
+    private lateinit var bucket: String
+
     fun uploadImage(image: MultipartFile): String {
         val fileName = image.originalFilename
-        val uploadFileName = System.currentTimeMillis().toString() + "_" + fileName
-        val metadata  = ObjectMetadata()
+        val metadata = ObjectMetadata()
         metadata.contentType = image.contentType
         metadata.contentLength = image.size
 
-        return try {
-            amazonS3Client.putObject(bucket, uploadFileName, image.inputStream, metadata)
-            val uploadFileURL = amazonS3Client.getUrl(bucket, uploadFileName)
-            uploadFileURL.toString()
-        } catch (e: Exception) {
-            "upload error"
-        }
+        amazonS3Client.putObject(bucket, fileName, image.inputStream, metadata)
+        val uploadFileURL = amazonS3Client.getUrl(bucket, fileName)
+        return uploadFileURL.toString()
+    }
+
+    fun downloadImage(imageUrl: String): ByteArray {
+        val fileName = imageUrl.split("/").last()
+        val s3Object = amazonS3Client.getObject(bucket, fileName)
+        return s3Object.objectContent.readAllBytes()
+    }
+
+    fun deleteImage(imageUrl: String) {
+        val fileName = imageUrl.split("/").last()
+        amazonS3Client.deleteObject(bucket, fileName)
     }
 }
