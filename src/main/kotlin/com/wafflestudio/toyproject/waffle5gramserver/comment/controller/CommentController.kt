73a720d17122f7952b2de9f9f55efbbe643266aa
rayark1 <max980203@snu.ac.kr>
2024-01-18 15:48:43 +0900

    style: ktlintFormat 적용

--- src/main/kotlin/com/wafflestudio/toyproject/waffle5gramserver/comment/controller/CommentController.kt
+++ src/main/kotlin/com/wafflestudio/toyproject/waffle5gramserver/comment/controller/CommentController.kt
@@ -1,11 +1,11 @@
 package com.wafflestudio.toyproject.waffle5gramserver.comment.controller
 
+import com.wafflestudio.toyproject.waffle5gramserver.comment.service.Comment
+import com.wafflestudio.toyproject.waffle5gramserver.comment.service.CommentAlreadyLikedException
 import com.wafflestudio.toyproject.waffle5gramserver.comment.service.CommentException
 import com.wafflestudio.toyproject.waffle5gramserver.comment.service.CommentNotFoundException
 import com.wafflestudio.toyproject.waffle5gramserver.comment.service.CommentNotLikedException
-import com.wafflestudio.toyproject.waffle5gramserver.comment.service.CommentAlreadyLikedException
 import com.wafflestudio.toyproject.waffle5gramserver.comment.service.CommentService
-import com.wafflestudio.toyproject.waffle5gramserver.comment.service.Comment
 import com.wafflestudio.toyproject.waffle5gramserver.comment.service.UserNotFoundException
 import com.wafflestudio.toyproject.waffle5gramserver.user.service.InstagramUser
 import org.springframework.data.domain.Page
@@ -13,15 +13,15 @@ import org.springframework.data.domain.Pageable
 import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
 import org.springframework.security.core.annotation.AuthenticationPrincipal
-import org.springframework.web.bind.annotation.ExceptionHandler
-import org.springframework.web.bind.annotation.RestController
-import org.springframework.web.bind.annotation.GetMapping
-import org.springframework.web.bind.annotation.PostMapping
-import org.springframework.web.bind.annotation.RequestMapping
-import org.springframework.web.bind.annotation.PathVariable
-import org.springframework.web.bind.annotation.RequestBody
-import org.springframework.web.bind.annotation.PutMapping
 import org.springframework.web.bind.annotation.DeleteMapping
+import org.springframework.web.bind.annotation.ExceptionHandler
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PathVariable
+import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.PutMapping
+import org.springframework.web.bind.annotation.RequestBody
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RestController
 
 @RestController
 @RequestMapping("/api/v1")
--- src/main/kotlin/com/wafflestudio/toyproject/waffle5gramserver/comment/service/CommentBrief.kt
+++
@@ -1,6 +0,0 @@
-package com.wafflestudio.toyproject.waffle5gramserver.comment.service
-
-data class CommentBrief(
-    val commentId: Long,
-    val content: String,
-)
