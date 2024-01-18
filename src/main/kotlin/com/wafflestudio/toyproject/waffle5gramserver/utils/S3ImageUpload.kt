c3a8272c09f037b4f300d3ac6ebe92f5fc2c9746
suhyuk <isuh88@snu.ac.kr>
2024-01-18 15:59:35 +0900

    fix: region 오류 수정중

--- src/main/kotlin/com/wafflestudio/toyproject/waffle5gramserver/utils/S3ImageUpload.kt
+++ src/main/kotlin/com/wafflestudio/toyproject/waffle5gramserver/utils/S3ImageUpload.kt
@@ -3,14 +3,15 @@ package com.wafflestudio.toyproject.waffle5gramserver.utils
 import com.amazonaws.services.s3.AmazonS3Client
 import com.amazonaws.services.s3.model.ObjectMetadata
 import org.springframework.beans.factory.annotation.Value
+import org.springframework.stereotype.Service
 import org.springframework.web.multipart.MultipartFile
 
+@Service
 class S3ImageUpload(
-    private val amazonS3Client: AmazonS3Client,
+    private val amazonS3Client: AmazonS3Client
 ) {
     @Value("\${cloud.aws.s3.bucket}")
     private lateinit var bucket: String
-
     fun uploadImage(image: MultipartFile): String {
         val fileName = image.originalFilename
         val metadata = ObjectMetadata()
