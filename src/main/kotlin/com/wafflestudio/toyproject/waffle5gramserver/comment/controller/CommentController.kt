cf9739a1c7c7e431845372aa377951ce0b7c412e
rayark1 <max980203@snu.ac.kr>
2024-01-18 15:34:44 +0900

    feat: comment handleException 추가

--- src/main/kotlin/com/wafflestudio/toyproject/waffle5gramserver/comment/controller/CommentController.kt
+++ src/main/kotlin/com/wafflestudio/toyproject/waffle5gramserver/comment/controller/CommentController.kt
@@ -1,21 +1,13 @@
 package com.wafflestudio.toyproject.waffle5gramserver.comment.controller
 
-import com.wafflestudio.toyproject.waffle5gramserver.comment.service.Comment
-import com.wafflestudio.toyproject.waffle5gramserver.comment.service.CommentService
+import com.wafflestudio.toyproject.waffle5gramserver.comment.service.*
 import com.wafflestudio.toyproject.waffle5gramserver.user.service.InstagramUser
 import org.springframework.data.domain.Page
 import org.springframework.data.domain.Pageable
 import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
 import org.springframework.security.core.annotation.AuthenticationPrincipal
-import org.springframework.web.bind.annotation.DeleteMapping
-import org.springframework.web.bind.annotation.GetMapping
-import org.springframework.web.bind.annotation.PathVariable
-import org.springframework.web.bind.annotation.PostMapping
-import org.springframework.web.bind.annotation.PutMapping
-import org.springframework.web.bind.annotation.RequestBody
-import org.springframework.web.bind.annotation.RequestMapping
-import org.springframework.web.bind.annotation.RestController
+import org.springframework.web.bind.annotation.*
 
 @RestController
 @RequestMapping("/api/v1")
@@ -59,6 +51,15 @@ class CommentController(
         commentService.delete(commentId, user.id)
         return ResponseEntity.noContent().build()
     }
+
+    @ExceptionHandler(CommentException::class)
+    fun handleException(e: CommentException): ResponseEntity<Any> {
+        return when (e) {
+            is CommentNotFoundException -> ResponseEntity.notFound().build()
+            is CommentNotLikedException -> ResponseEntity.status(HttpStatus.FORBIDDEN).build()
+            is CommentAlreadyLikedException -> ResponseEntity.status(HttpStatus.CONFLICT).build()
+            is UserNotFoundException -> ResponseEntity.notFound().build()
+            else -> ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).build()
+        }
+    }
 }
-
-
