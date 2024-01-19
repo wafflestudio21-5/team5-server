d7739cbf6a8816482c696b5d5d43c83f56c21c75
suhyuk <isuh88@snu.ac.kr>
2024-01-20 02:03:36 +0900

    test: 통합 테스트 로직 작성

--- src/test/kotlin/com/wafflestudio/toyproject/waffle5gramserver/feed/FeedIntegrationTest.kt
+++ src/test/kotlin/com/wafflestudio/toyproject/waffle5gramserver/feed/FeedIntegrationTest.kt
@@ -1,19 +1,44 @@
 package com.wafflestudio.toyproject.waffle5gramserver.feed
 
-import com.wafflestudio.toyproject.waffle5gramserver.feed.service.FeedService
-import com.wafflestudio.toyproject.waffle5gramserver.post.repository.PostRepository
-import org.junit.jupiter.api.BeforeAll
-import org.junit.jupiter.api.Test
+성import org.junit.jupiter.api.Test
 import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc
 import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.http.MediaType
+import org.springframework.test.web.servlet.MockMvc
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers.status
+import org.springframework.transaction.annotation.Transactional
 
-@SpringBootTest
+@Transactional
+@AutoConfigureMockMvc
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 class FeedIntegrationTest @Autowired constructor(
-    private val feedService: FeedService,
-    private val postRepository: PostRepository,
+    private val mvc: MockMvc
 ) {
     @Test
-    fun `test`() {
-        println("test")
+    fun `피드 테스트`() {
+        val token = obtainBearerToken("user-0", "password-0")
+
+        mvc.perform(
+            MockMvcRequestBuilders.get("/api/v1/feed/timeline")
+                .header("Authorization", "Bearer $token")
+                .param("page", "1")
+                .param("size", "10")
+                .contentType(MediaType.APPLICATION_JSON)
+        )
+            .andExpect(status().isOk)
+    }
+
+    private fun obtainBearerToken(username: String, password: String): String {
+        val response = mvc.perform(
+            MockMvcRequestBuilders.post("/api/v1/auth/login")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content("""{"username": "$username", "password": "$password"}""")
+        )
+            .andExpect(status().isCreated)
+            .andReturn()
+
+        return response.response.contentAsString.substringAfter(":").substringBefore("}").trim('"')
     }
 }
