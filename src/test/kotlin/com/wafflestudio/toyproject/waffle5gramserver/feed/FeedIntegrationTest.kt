25a149b01013dd81491fb2db1e719e10725a33ea
suhyuk <isuh88@snu.ac.kr>
2024-01-20 02:04:17 +0900

    typo: 오탈자 해결

--- src/test/kotlin/com/wafflestudio/toyproject/waffle5gramserver/feed/FeedIntegrationTest.kt
+++ src/test/kotlin/com/wafflestudio/toyproject/waffle5gramserver/feed/FeedIntegrationTest.kt
@@ -1,6 +1,6 @@
 package com.wafflestudio.toyproject.waffle5gramserver.feed
 
-성import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.Test
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc
 import org.springframework.boot.test.context.SpringBootTest
