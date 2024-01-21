e9608223c6954de4aef8cd808e97c57bda938015
suhyuk <isuh88@snu.ac.kr>
2024-01-21 17:30:03 +0900

    feat: user, like 반영 페이징

--- src/main/kotlin/com/wafflestudio/toyproject/waffle5gramserver/feed/service/FeedService.kt
+++ src/main/kotlin/com/wafflestudio/toyproject/waffle5gramserver/feed/service/FeedService.kt
@@ -3,8 +3,9 @@ package com.wafflestudio.toyproject.waffle5gramserver.feed.service
 import com.wafflestudio.toyproject.waffle5gramserver.post.service.PostDetail
 import org.springframework.data.domain.Page
 import org.springframework.data.domain.Pageable
+import org.springframework.data.domain.Slice
 
 interface FeedService {
-    fun getHomeFeed(userId: Long, pageable: Pageable): Page<PostDetail>
+    fun getHomeFeed(userId: Long, pageable: Pageable): Slice<PostDetail>
     fun getFeedWithPostId(postId: Long): List<PostDetail>
 }
--- src/main/kotlin/com/wafflestudio/toyproject/waffle5gramserver/feed/service/FeedServiceImpl.kt
+++ src/main/kotlin/com/wafflestudio/toyproject/waffle5gramserver/feed/service/FeedServiceImpl.kt
@@ -1,30 +1,40 @@
 package com.wafflestudio.toyproject.waffle5gramserver.feed.service
 
+import com.wafflestudio.toyproject.waffle5gramserver.feed.repository.FeedQueryRepository
 import com.wafflestudio.toyproject.waffle5gramserver.post.mapper.PostMapper
 import com.wafflestudio.toyproject.waffle5gramserver.post.repository.PostRepository
 import com.wafflestudio.toyproject.waffle5gramserver.post.service.PostDetail
 import com.wafflestudio.toyproject.waffle5gramserver.post.service.PostLikeService
 import org.springframework.data.domain.Page
 import org.springframework.data.domain.Pageable
+import org.springframework.data.domain.Slice
+import org.springframework.data.domain.SliceImpl
 import org.springframework.stereotype.Service
 import org.springframework.transaction.annotation.Transactional
 
 @Service
 class FeedServiceImpl(
     private val postRepository: PostRepository,
-    private val postLikeService: PostLikeService
+    private val postLikeService: PostLikeService,
+    private val feedQueryRepository: FeedQueryRepository
 ) : FeedService {
 
     @Transactional(readOnly = true)
-    override fun getHomeFeed(userId: Long, pageable: Pageable): Page<PostDetail> {
-        val postsList = postRepository.findAllByUserIdIsNotOrderByCreatedAtDesc(userId, pageable)
+    override fun getHomeFeed(userId: Long, pageable: Pageable): Slice<PostDetail> {
+        val postsList = feedQueryRepository.findPostsNotCreatedByAndNotLikedByUser(userId, pageable)
 
+        val sliceInfo = postsList.pageable
         val postDetailsList = postsList.map { post ->
-            val liked = postLikeService.exists(post.id, userId)
-            PostMapper.toPostDetailDTO(post, liked)
-        }
+            PostMapper.toPostDetailDTO(post)
+        }.toList()
 
-        return postDetailsList
+        val shuffledPostDetailsList = postDetailsList.shuffled()
+
+        return SliceImpl(
+            shuffledPostDetailsList,
+            sliceInfo,
+            postsList.hasNext()
+        )
     }
 
     override fun getFeedWithPostId(postId: Long): List<PostDetail> {
