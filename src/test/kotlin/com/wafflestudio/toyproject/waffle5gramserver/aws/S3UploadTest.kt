a08e68c543c0fb093ddeb8fcbd73cc3c492935ac
suhyuk <isuh88@snu.ac.kr>
2024-01-21 22:37:05 +0900

    test: 이미지 업로드 테스트

--- src/test/kotlin/com/wafflestudio/toyproject/waffle5gramserver/aws/S3UploadTest.kt
+++ src/test/kotlin/com/wafflestudio/toyproject/waffle5gramserver/aws/S3UploadTest.kt
@@ -2,6 +2,7 @@ package com.wafflestudio.toyproject.waffle5gramserver.aws;
 
 import com.amazonaws.services.s3.AmazonS3Client
 import com.wafflestudio.toyproject.waffle5gramserver.utils.S3ImageUpload
+import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.Assertions.assertNotNull
 import org.junit.jupiter.api.Test
 import org.springframework.beans.factory.annotation.Autowired
@@ -15,8 +16,8 @@ class S3UploadTest @Autowired constructor(
     private val amazonS3Client: AmazonS3Client
 ) {
     private fun createImage(): MultipartFile {
-        val filePath = Paths.get("src/test/resources/likelion.png")
-        val originalFilename = "likelion.png"
+        val filePath = Paths.get("src/test/resources/spring.jpg")
+        val originalFilename = "spring.jpg"
         val contentType = "image/png"
         val content = filePath.toFile().readBytes()
         return MockMultipartFile(
@@ -40,15 +41,15 @@ class S3UploadTest @Autowired constructor(
     }
 
     @Test
-    fun `이미지 가져오기`() {
+    fun `이미지 삭제`() {
         // Given
         val image = createImage()
         val uploadedUrl = S3ImageUpload(amazonS3Client).uploadImage(image)
 
         // When
-        val downloadedImage = S3ImageUpload(amazonS3Client).downloadImage(uploadedUrl)
+        val deletedImage = S3ImageUpload(amazonS3Client).deleteImage(uploadedUrl)
 
         // Then
-        assertNotNull(downloadedImage)
+        assertEquals(deletedImage, image.originalFilename)
     }
 }
