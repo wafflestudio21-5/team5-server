77d2dd5d3506a7a262e669211dd6dbd9f5087a79
Suhyuk <105275625+isuh88@users.noreply.github.com>
2024-01-21 20:06:15 +0900

    feat/feedv2: 홈 피드 구성 관련 기능 (#8)
    * feat: 피드 관련 로직 작성
    
    * fix: 게시글 DTO 변경 및 레포지토리 함수 추가
    
    * chore: PageInfo 패키지 변경
    
    * test: 피드 테스트 코드
    
    * conf: 스프링 프로필 변경
    
    * fix: PageInfo 패키지 변경 반영
    
    * fix: PR 피드백 및 main 반영
    
    * test: 통합 테스트 로직 작성
    
    * typo: 오탈자 해결
    
    * chore: 페이지 인덱스 1부터 시작 설정
    
    * test: 피드 api http 리퀘스트
    
    * feat: user, like 반영 페이징
    
    * fix: 명세에 맞게 DTO 변경, 응답 객체 변경
    
    * test: 피드 조회 테스트
    
    * conf: dev 프로필 인덱스 설정 변경
    
    * test: 트랜잭션 어노테이션 추가
    
    * test: 트랜잭션 전파 문제 해결
    
    * test: assertion 임시 주석 처리
    
    * fix: ktlint 수정

---
+++ src/test/kotlin/com/wafflestudio/toyproject/waffle5gramserver/feed/FeedIntegrationTest.kt
@@ -0,0 +1,45 @@
+package com.wafflestudio.toyproject.waffle5gramserver.feed
+
+import org.junit.jupiter.api.Test
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc
+import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.http.MediaType
+import org.springframework.test.web.servlet.MockMvc
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers.status
+import org.springframework.transaction.annotation.Transactional
+
+@Transactional
+@AutoConfigureMockMvc
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+class FeedIntegrationTest @Autowired constructor(
+    private val mvc: MockMvc
+) {
+    @Transactional
+    @Test
+    fun `피드 테스트`() {
+        val token = obtainBearerToken("user-0", "password-0")
+
+        mvc.perform(
+            MockMvcRequestBuilders.get("/api/v1/feed/timeline")
+                .header("Authorization", "Bearer $token")
+                .param("page", "1")
+                .param("size", "10")
+                .contentType(MediaType.APPLICATION_JSON)
+        )
+            .andExpect(status().isOk)
+    }
+
+    private fun obtainBearerToken(username: String, password: String): String {
+        val response = mvc.perform(
+            MockMvcRequestBuilders.post("/api/v1/auth/login")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content("""{"username": "$username", "password": "$password"}""")
+        )
+            .andExpect(status().isCreated)
+            .andReturn()
+
+        return response.response.contentAsString.substringAfter(":").substringBefore("}").trim('"')
+    }
+}
