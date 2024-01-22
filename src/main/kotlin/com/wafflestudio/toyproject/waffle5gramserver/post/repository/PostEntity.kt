c24805b13088e8e93ed53d81d8d6e676c13f543a
suhyuk <isuh88@snu.ac.kr>
2024-01-23 02:58:48 +0900

    fix: BaseEntity 상속을 통해 변경 감지로 수정 일시 저장 반영

--- src/main/kotlin/com/wafflestudio/toyproject/waffle5gramserver/post/repository/PostEntity.kt
+++ src/main/kotlin/com/wafflestudio/toyproject/waffle5gramserver/post/repository/PostEntity.kt
@@ -1,5 +1,6 @@
 package com.wafflestudio.toyproject.waffle5gramserver.post.repository
 
+import com.wafflestudio.toyproject.waffle5gramserver.BaseModificationAuditingEntity
 import com.wafflestudio.toyproject.waffle5gramserver.comment.repository.CommentEntity
 import com.wafflestudio.toyproject.waffle5gramserver.user.repository.UserEntity
 import jakarta.persistence.CascadeType
@@ -18,21 +19,12 @@ import java.time.LocalDateTime
 
 @Entity(name = "posts")
 class PostEntity(
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    var id: Long = 0L,
     @Column(name = "content", nullable = false)
     var content: String = "",
     @Column(name = "like_count_displayed", nullable = false)
     var likeCountDisplayed: Boolean = true,
     @Column(name = "comment_displayed", nullable = false)
     var commentDisplayed: Boolean = true,
-    @CreatedDate
-    @Column(name = "created_at", nullable = false, updatable = false)
-    var createdAt: LocalDateTime = LocalDateTime.now(),
-    @UpdateTimestamp
-    @Column(name = "modified_at", nullable = false)
-    var modifiedAt: LocalDateTime = LocalDateTime.now(),
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "user_id", nullable = false)
     var user: UserEntity,
@@ -40,7 +32,7 @@ class PostEntity(
     var medias: MutableList<PostMediaEntity> = mutableListOf(),
     @OneToMany(mappedBy = "post", cascade = [CascadeType.ALL], orphanRemoval = true)
     var comments: MutableList<CommentEntity> = mutableListOf(),
-) {
+) : BaseModificationAuditingEntity() {
     fun addMedia(media: PostMediaEntity) {
         medias.add(media)
     }
