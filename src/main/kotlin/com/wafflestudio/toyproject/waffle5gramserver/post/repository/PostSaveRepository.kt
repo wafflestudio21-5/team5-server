cad15cffbcd5649208e81b94f1bcf4d9a8b8abd3
rayark1 <max980203@snu.ac.kr>
2024-01-23 11:13:35 +0900

    feat: PostSave create, delete, exists 서비스 구현

---
+++ src/main/kotlin/com/wafflestudio/toyproject/waffle5gramserver/post/repository/PostSaveRepository.kt
@@ -0,0 +1,10 @@
+package com.wafflestudio.toyproject.waffle5gramserver.post.repository
+
+import org.springframework.data.jpa.repository.JpaRepository
+
+interface PostSaveRepository : JpaRepository<PostSaveEntity, Long> {
+    fun findByUserIdAndPostId(
+        userId: Long,
+        postId: Long,
+    ): PostSaveEntity?
+}
