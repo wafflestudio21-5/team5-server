4de105b2845138cae0418ac5098400ec248e494d
jj1kim <jeewonbob@gmail.com>
2024-01-24 02:11:25 +0900

    feat: FollowRepository 및 FollowRequestRepository 레이아웃 작성

---
+++ src/main/kotlin/com/wafflestudio/toyproject/waffle5gramserver/follow/repository/FollowRepository.kt
@@ -0,0 +1,6 @@
+package com.wafflestudio.toyproject.waffle5gramserver.follow.repository
+
+import org.springframework.data.jpa.repository.JpaRepository
+
+interface FollowRepository: JpaRepository<FollowEntity,Long> {
+}
