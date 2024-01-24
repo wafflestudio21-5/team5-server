a2faf3c021b4959bb6aad03212d4e716702098f2
jj1kim <jeewonbob@gmail.com>
2024-01-24 22:36:18 +0900

    Feat : UserLinkEntity와 UserLinkRepository 작성

---
+++ src/main/kotlin/com/wafflestudio/toyproject/waffle5gramserver/user/repository/UserLinkEntity.kt
@@ -0,0 +1,17 @@
+package com.wafflestudio.toyproject.waffle5gramserver.user.repository
+
+import com.wafflestudio.toyproject.waffle5gramserver.BaseAuditingEntity
+import jakarta.persistence.Entity
+import jakarta.persistence.EnumType
+import jakarta.persistence.FetchType
+import jakarta.persistence.JoinColumn
+import jakarta.persistence.ManyToOne
+
+@Entity(name = "user_links")
+class UserLinkEntity(
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "userId", nullable = false)
+    val user: UserEntity,
+    val link: String,
+    val linkTitle: String,
+) : BaseAuditingEntity()
---
+++ src/main/kotlin/com/wafflestudio/toyproject/waffle5gramserver/user/repository/UserLinkRepository.kt
@@ -0,0 +1,7 @@
+package com.wafflestudio.toyproject.waffle5gramserver.user.repository
+
+import org.springframework.data.jpa.repository.JpaRepository
+
+interface UserLinkRepository : JpaRepository<UserLinkEntity,Long> {
+
+}
