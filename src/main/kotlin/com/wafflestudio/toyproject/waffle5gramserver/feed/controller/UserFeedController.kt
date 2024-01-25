ed48e5c7f97fe6be4ba9959ca3b9181acfcbce71
김지원(Jiwon Kim) <134778013+jj1kim@users.noreply.github.com>
2024-01-26 04:27:33 +0900

    fix : UserFeedRetrieve 관련 api 3개에 로그인 유저 의존성 및 에러 추가 (#32)

--- src/main/kotlin/com/wafflestudio/toyproject/waffle5gramserver/feed/controller/UserFeedController.kt
+++ src/main/kotlin/com/wafflestudio/toyproject/waffle5gramserver/feed/controller/UserFeedController.kt
@@ -3,7 +3,9 @@ package com.wafflestudio.toyproject.waffle5gramserver.feed.controller
 import com.wafflestudio.toyproject.waffle5gramserver.feed.service.PostPreview
 import com.wafflestudio.toyproject.waffle5gramserver.feed.service.UserFeedService
 import com.wafflestudio.toyproject.waffle5gramserver.post.service.PostDetail
+import com.wafflestudio.toyproject.waffle5gramserver.user.service.InstagramUser
 import org.springframework.http.ResponseEntity
+import org.springframework.security.core.annotation.AuthenticationPrincipal
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.RequestMapping
@@ -18,11 +20,12 @@ class UserFeedController(
     // 1. 유저의 게시물 미리보기 조회 API
     @GetMapping("/preview")
     fun getUserFeedPreview(
+        @AuthenticationPrincipal authuser: InstagramUser,
         @PathVariable username: String,
         @RequestParam(required = false) cursor: Long?,
         @RequestParam(defaultValue = "12") limit: Int,
     ): ResponseEntity<List<PostPreview>> {
-        val postPreviews = userFeedService.getUserFeedPreview(username, cursor, limit)
+        val postPreviews = userFeedService.getUserFeedPreview(authuser, username, cursor, limit)
         return ResponseEntity.ok(postPreviews)
     }
 
@@ -30,22 +33,24 @@ class UserFeedController(
     // 위로 스크롤하여 최신 게시물 로드
     @GetMapping("/newer")
     fun loadNewerPosts(
+        @AuthenticationPrincipal authuser: InstagramUser,
         @PathVariable username: String,
         @RequestParam(required = false) cursor: Long?,
         @RequestParam(defaultValue = "10") limit: Int,
     ): ResponseEntity<List<PostDetail>> {
-        val newerPosts = userFeedService.loadNewerPosts(username, cursor, limit)
+        val newerPosts = userFeedService.loadNewerPosts(authuser, username, cursor, limit)
         return ResponseEntity.ok(newerPosts)
     }
 
     // 아래로 스크롤하여 이전 게시물 로드
     @GetMapping("/older")
     fun loadOlderPosts(
+        @AuthenticationPrincipal authuser: InstagramUser,
         @PathVariable username: String,
         @RequestParam(required = false) cursor: Long?,
         @RequestParam(defaultValue = "10") limit: Int,
     ): ResponseEntity<List<PostDetail>> {
-        val olderPosts = userFeedService.loadOlderPosts(username, cursor, limit)
+        val olderPosts = userFeedService.loadOlderPosts(authuser, username, cursor, limit)
         return ResponseEntity.ok(olderPosts)
     }
 }
