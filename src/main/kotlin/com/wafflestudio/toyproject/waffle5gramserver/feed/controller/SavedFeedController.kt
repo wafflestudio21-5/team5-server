6630030287c892d28319b53db94b3638aaf6fe3e
jj1kim <jeewonbob@gmail.com>
2024-01-26 14:51:22 +0900

    fix : saved post api 3개의 username을 삭제하고 현재 로그인한 유저로 대체

--- src/main/kotlin/com/wafflestudio/toyproject/waffle5gramserver/feed/controller/SavedFeedController.kt
+++ src/main/kotlin/com/wafflestudio/toyproject/waffle5gramserver/feed/controller/SavedFeedController.kt
@@ -3,45 +3,46 @@ package com.wafflestudio.toyproject.waffle5gramserver.feed.controller
 import com.wafflestudio.toyproject.waffle5gramserver.feed.service.PostPreview
 import com.wafflestudio.toyproject.waffle5gramserver.feed.service.SavedFeedService
 import com.wafflestudio.toyproject.waffle5gramserver.post.service.PostDetail
+import com.wafflestudio.toyproject.waffle5gramserver.user.service.InstagramUser
 import org.springframework.http.ResponseEntity
+import org.springframework.security.core.annotation.AuthenticationPrincipal
 import org.springframework.web.bind.annotation.GetMapping
-import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RequestParam
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
-@RequestMapping("/api/v1/account/{username}/saved-feed")
+@RequestMapping("/api/v1/account/saved-feed")
 class SavedFeedController(
     private val savedFeedService: SavedFeedService,
 ) {
     @GetMapping("/preview")
     fun getSavedFeedPreview(
-        @PathVariable username: String,
+        @AuthenticationPrincipal authuser: InstagramUser,
         @RequestParam(required = false) cursor: Long?,
         @RequestParam(defaultValue = "12") limit: Int,
     ): ResponseEntity<List<PostPreview>> {
-        val postPreviews = savedFeedService.getSavedFeedPreview(username, cursor, limit)
+        val postPreviews = savedFeedService.getSavedFeedPreview(authuser.username, cursor, limit)
         return ResponseEntity.ok(postPreviews)
     }
 
     @GetMapping("/newer")
     fun loadNewerPosts(
-        @PathVariable username: String,
+        @AuthenticationPrincipal authuser: InstagramUser,
         @RequestParam(required = false) cursor: Long?,
         @RequestParam(defaultValue = "10") limit: Int,
     ): ResponseEntity<List<PostDetail>> {
-        val newerPosts = savedFeedService.loadNewerPosts(username, cursor, limit)
+        val newerPosts = savedFeedService.loadNewerPosts(authuser.username, cursor, limit)
         return ResponseEntity.ok(newerPosts)
     }
 
     @GetMapping("/older")
     fun loadOlderPosts(
-        @PathVariable username: String,
+        @AuthenticationPrincipal authuser: InstagramUser,
         @RequestParam(required = false) cursor: Long?,
         @RequestParam(defaultValue = "10") limit: Int,
     ): ResponseEntity<List<PostDetail>> {
-        val olderPosts = savedFeedService.loadOlderPosts(username, cursor, limit)
+        val olderPosts = savedFeedService.loadOlderPosts(authuser.username, cursor, limit)
         return ResponseEntity.ok(olderPosts)
     }
 }
