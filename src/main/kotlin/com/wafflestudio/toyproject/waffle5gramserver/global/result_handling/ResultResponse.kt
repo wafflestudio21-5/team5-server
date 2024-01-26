ed9464172d3e1e5b8f14671de40fb908f646c2f6
jj1kim <jeewonbob@gmail.com>
2024-01-26 20:22:36 +0900

    fix : response convention에서 result code와 message를 함께 내려주는 것 삭제

--- src/main/kotlin/com/wafflestudio/toyproject/waffle5gramserver/global/result_handling/ResultResponse.kt
+++ src/main/kotlin/com/wafflestudio/toyproject/waffle5gramserver/global/result_handling/ResultResponse.kt
@@ -3,15 +3,15 @@ package com.wafflestudio.toyproject.waffle5gramserver.global.result_handling
 // ErrorResponse처럼 성공적인 결과 응답에 대한 획일화된 데이터 모델을 정의합니다.
 class ResultResponse {
 
-    var status: Int // Http 상태 코드
+    /*var status: Int // Http 상태 코드
     var code: String // Business 상태 코드
-    var message: String // 응답 메시지
+    var message: String // 응답 메시지*/
     var data: Any // 응답 데이터
 
     constructor(resultCode: ResultCode, data: Any) {
-        this.status = resultCode.status
+        /*this.status = resultCode.status
         this.code = resultCode.code
-        this.message = resultCode.message
+        this.message = resultCode.message*/
         this.data = data
     }
 
