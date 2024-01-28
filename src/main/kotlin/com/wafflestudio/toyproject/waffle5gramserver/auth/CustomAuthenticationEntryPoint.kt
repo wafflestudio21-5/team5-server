70029ad1bec0de5a8baf43641a0faca0ca17e917
seonghaejo <jsh990324@snu.ac.kr>
2024-01-28 14:45:17 +0900

    feat: custom AuthenticationEntryPoint & AccessDeniedHandler

---
+++ src/main/kotlin/com/wafflestudio/toyproject/waffle5gramserver/auth/CustomAuthenticationEntryPoint.kt
@@ -0,0 +1,31 @@
+package com.wafflestudio.toyproject.waffle5gramserver.auth
+
+import com.fasterxml.jackson.databind.ObjectMapper
+import jakarta.servlet.http.HttpServletRequest
+import jakarta.servlet.http.HttpServletResponse
+import org.springframework.http.MediaType
+import org.springframework.security.core.AuthenticationException
+import org.springframework.security.web.AuthenticationEntryPoint
+import org.springframework.stereotype.Component
+
+@Component
+class CustomAuthenticationEntryPoint(
+    private val objectMapper: ObjectMapper
+) : AuthenticationEntryPoint {
+    override fun commence(
+        request: HttpServletRequest?,
+        response: HttpServletResponse?,
+        authException: AuthenticationException?
+    ) {
+        authException?.printStackTrace()
+        response?.let {
+            it.contentType = MediaType.APPLICATION_JSON_VALUE
+            it.status = HttpServletResponse.SC_UNAUTHORIZED
+            it.writer.write(
+                objectMapper.writeValueAsString(
+                    mapOf("error" to "Authentication Exception from Custom Authentication Entry Point")
+                )
+            )
+        }
+    }
+}
