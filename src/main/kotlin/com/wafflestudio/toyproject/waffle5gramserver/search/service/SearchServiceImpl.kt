9154fa9971bc740cc0df835a156fb25d7e96e050
jj1kim <jeewonbob@gmail.com>
2024-02-01 01:26:56 +0900

    feat: SearchService 틀 작성

---
+++ src/main/kotlin/com/wafflestudio/toyproject/waffle5gramserver/search/service/SearchServiceImpl.kt
@@ -0,0 +1,14 @@
+package com.wafflestudio.toyproject.waffle5gramserver.search.service
+
+import com.wafflestudio.toyproject.waffle5gramserver.search.repository.RecentSearchEntity
+import com.wafflestudio.toyproject.waffle5gramserver.search.repository.RecentSearchRepository
+import com.wafflestudio.toyproject.waffle5gramserver.user.repository.UserRepository
+import org.springframework.stereotype.Service
+
+@Service
+class SearchServiceImpl (
+    private val userRepository: UserRepository,
+    private val recentSearchRepository: RecentSearchRepository,
+) : SearchService {
+
+}
