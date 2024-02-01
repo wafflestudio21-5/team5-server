f456bbf0bbb1cd804b838252cc89cf36d8304dfb
jj1kim <jeewonbob@gmail.com>
2024-02-01 10:40:25 +0900

    fix : 쿼리 범위 수정

--- src/main/kotlin/com/wafflestudio/toyproject/waffle5gramserver/user/repository/UserRepository.kt
+++ src/main/kotlin/com/wafflestudio/toyproject/waffle5gramserver/user/repository/UserRepository.kt
@@ -57,14 +57,5 @@ interface UserRepository : JpaRepository<UserEntity, Long> {
     )
     fun findAllByText(text: String): MutableList<UserEntity>
 
-//    @Modifying
-//    @Query(
-//        """
-//        SELECT u FROM users u
-//        WHERE u.username LIKE %:text%
-//        OR u.name LIKE %:text%"""
-//    )
-//    fun findAllPagingByText(text: String, pageable: Pageable): Page<UserEntity>
-
-    fun findAllByUsernameContaining(text: String, pageable: Pageable): Page<UserEntity>
+    fun findAllByUsernameContainingOrNameContaining(text: String, pageable: Pageable): Page<UserEntity>
 }
