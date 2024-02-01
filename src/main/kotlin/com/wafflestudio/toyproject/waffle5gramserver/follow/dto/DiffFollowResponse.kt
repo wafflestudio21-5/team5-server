ad2cc5bebb15097b4d012058d955f66d6f9cb236
김지원(Jiwon Kim) <134778013+jj1kim@users.noreply.github.com>
2024-02-02 00:21:45 +0900

    fix : diff 목록 조회 api에 isRequest 추가 (#75)

--- src/main/kotlin/com/wafflestudio/toyproject/waffle5gramserver/follow/dto/DiffFollowResponse.kt
+++ src/main/kotlin/com/wafflestudio/toyproject/waffle5gramserver/follow/dto/DiffFollowResponse.kt
@@ -1,8 +1,6 @@
 package com.wafflestudio.toyproject.waffle5gramserver.follow.dto
 
-import com.wafflestudio.toyproject.waffle5gramserver.user.dto.MiniProfile
-
 data class DiffFollowResponse(
     val count: Long,
-    val miniProfiles: MutableList<MiniProfile>,
+    val miniProfiles: MutableList<IsRequestMiniProfile>,
 )
